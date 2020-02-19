07d0ddcd6c0050e0849b28eba6ec487e36fffcc5
Jônatas Santos <jonatasricardo90@gmail.com>
2020-02-19 14:46:32 -0300

    offset

---
+++ .gitignore/offset.js
@@ -0,0 +1,9 @@
+// correct offset calculation
+function offset(elem) {
+    const rect = elem.getBoundingClientRect();
+    const win = elem.ownerDocument.defaultView;
+    return {
+        top: rect.top + win.pageYOffset,
+        left: rect.left + win.pageXOffset
+    };
+}
